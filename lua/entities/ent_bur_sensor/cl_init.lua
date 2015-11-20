61f5dd9448be35001c59386e9f8b7f624618f8bd
BurgerLUA <mattwasnothere@gmail.com>
2015-11-19 21:12:36 -0800

    Update

--- lua/entities/ent_bur_sensor/cl_init.lua
+++ lua/entities/ent_bur_sensor/cl_init.lua
@@ -8,24 +8,19 @@ local NextThink = 0
 local Mat = Material("sprites/bomb_planted_ring")
 
 function ENT:Think()
-
-	if NextThink <= CurTime() then
-		EmitSound("common/stuck1.wav",self:GetPos(),self:EntIndex(),CHAN_AUTO,1,75,0,100)
-		NextThink = CurTime() + 1
+	if self:GetVelocity():Length() < 1 then
+		if NextThink <= CurTime() then
+			EmitSound("common/stuck1.wav",self:GetPos(),self:EntIndex(),CHAN_AUTO,1,75,0,100)
+			NextThink = CurTime() + 1
+		end
 	end
-
 end
 
-function ENT:Draw()
-	
+function ENT:Draw()	
 	self:DrawModel()
-
 	if self:GetVelocity():Length() < 1 then
-	
 		local Pos = self:GetPos() + self:OBBCenter()
-	
 		render.SetMaterial(Mat)
 		render.DrawSprite(Pos,5,5,Color(255,0,0,255))
 	end
-
 end
