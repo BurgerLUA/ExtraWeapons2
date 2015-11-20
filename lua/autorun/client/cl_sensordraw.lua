7d7f1e38ed9eb971617cffd81a59d9505404c625
BurgerLUA <mattwasnothere@gmail.com>
2015-11-19 21:23:19 -0800

    Updated 2

--- lua/autorun/client/cl_sensordraw.lua
+++ lua/autorun/client/cl_sensordraw.lua
@@ -1,20 +1,14 @@
 local DrawMaterial = "burger/ignorez"
 
 function SensorHandleDrawing(ply)
-	
-	--print("test")
 
 	if not IsValid(ply) then return end
-	--if not ply == LocalPlayer() then return end
 	if not ply:Alive() then return end
-
 	
 	local IsInSensor = false
-
-	--print("test")
 	
 	for k,v in pairs(ents.FindByClass("ent_bur_sensor")) do
-
+	
 		local Distance = ply:GetPos():Distance(v:GetPos())
 		local EntSpeed = v:GetVelocity():Length()
 		local PlySpeed = ply:GetVelocity():Length()
@@ -22,23 +16,15 @@ function SensorHandleDrawing(ply)
 		if Distance <= 256 and EntSpeed < 1 and PlySpeed > 1 then
 			IsInSensor = true
 		end
-
-	end
-	
-	--PrintTable(ply:GetMaterials())
-	
-	if IsInSensor == true and ply:GetMaterial() ~= DrawMaterial then
-		--ply:DrawModel()
-		ply:SetMaterial(DrawMaterial)
-		--print(ply:Nick(), "In Sensor")
 		
-	else--if ply:GetMaterial() == DrawMaterial then
-		ply:SetMaterial("")
-		--print("BAD")
+	end
+	
+	if IsInSensor == true then
+		ply:SetSubMaterial(0,DrawMaterial)
+	elseif IsInSensor == false then
+		ply:SetSubMaterial(0,nil)
 	end
 
-	
-	
 end
 
 hook.Add("PostPlayerDraw","Sensor Drawer",SensorHandleDrawing)
