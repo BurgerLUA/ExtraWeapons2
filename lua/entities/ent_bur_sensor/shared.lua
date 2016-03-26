f6cecdf41890e7cdaf7a9f66c68c3eb5a797c360
BurgerLUA <mattwasnothere@gmail.com>
2016-03-25 19:36:39 -0700

    Fixed Sounds

--- lua/entities/ent_bur_sensor/shared.lua
+++ lua/entities/ent_bur_sensor/shared.lua
@@ -1,8 +1,8 @@
 ENT.Type = "anim"
 ENT.Base = "base_entity"
-ENT.PrintName = "Launcher smoke grenade"
-ENT.Author = "L337N008"
-ENT.Information = "A 40MM grenade launched from an underslung grenade launcher M203"
+ENT.PrintName = "Sensor Launcher"
+ENT.Author = ""
+ENT.Information = ""
 ENT.Spawnable = false
 ENT.AdminSpawnable = false 
 
