5f6241bcb76f916b88e7ffc508a35ecf49170356
BurgerLUA <mattwasnothere@gmail.com>
2016-04-16 21:57:59 -0700

    AAAA

--- lua/entities/ent_bur_naughty/init.lua
+++ lua/entities/ent_bur_naughty/init.lua
@@ -60,7 +60,7 @@ function ENT:PhysicsCollide(data, physobj)
 			data.HitEntity:Fire("Break")
 		end
 		
-		local Damage = (20+(data.OurOldVelocity:Length()/200)^1.5)*0.4*0.75
+		local Damage = (20+(data.OurOldVelocity:Length()/200)^1.5)*0.4*0.8
 		
 		if data.HitEntity:Health() >= 1 then
 			if data.HitEntity:IsPlayer() then 
