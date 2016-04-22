2654356ebdd9b803a31a6fece4e9719582b44491
BurgerLUA <mattwasnothere@gmail.com>
2016-04-21 20:33:02 -0700

    Slam Update

--- lua/entities/ent_bur_slam/init.lua
+++ lua/entities/ent_bur_slam/init.lua
@@ -169,17 +169,17 @@ function ENT:Detonate(self,pos)
 	util.Effect( "Explosion", effectdata )	
 	util.BlastDamage(self, self.FakeOwner, pos, 250, 250)
 	
-	--[[
+	
 	if table.Count(ents.FindInSphere(self:GetPos(),250)) > 0 then
 		for k,v in pairs(ents.FindInSphere(self:GetPos(),250)) do
 		
 			if v:GetClass() == "prop_physics" then
 		
-				
+				--[[
 				if math.Rand(0,100) >= 70 then
 					v:Ignite(250/20 - v:GetPos():Distance( self:GetPos() )/20,0)
 				end
-				
+				--]]
 				
 				timer.Simple(0,function() 
 					if v:IsValid() == false then return end
@@ -198,7 +198,7 @@ function ENT:Detonate(self,pos)
 		end
 
 	end
-	--]]
+
 					
 	self.Pos1 = self.HitP + self.HitN
 	self.Pos2 = self.HitP - self.HitN
